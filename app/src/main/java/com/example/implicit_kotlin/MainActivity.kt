7e42a0e227a3d350d6881f23ed51575ac9260902
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-14 20:23:08 +0600

    Redirect to browser

--- app/src/main/java/com/example/implicit_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/implicit_kotlin/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.implicit_kotlin
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button : Button =findViewById(R.id.btn)
+        button.setOnClickListener(){
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("https://www.google.com"))
+            startActivity(intent)
+        }
     }
 }
